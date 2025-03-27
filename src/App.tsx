9b4f7bc193fb7e70809b27dafba6c4a784ab26d8
digbanmije18@gmail.com <digbanmije18@gmail.com>
2025-03-27 20:52:31 +0100

    vhanges

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
+
 const products = [
-  { id: 1, name: "Sardine", packed: false, quantity: 3 },
+  { id: 1, description: "Sardine", packed: false, quantity: 3 },
   {
     id: 2,
-    name: "shoes",
+
+    description: "shoes",
     packed: false,
     quantity: 5,
   },
   {
     id: 3,
-    name: "clothes",
+    description: "clothes",
     packed: false,
     quantity: 15,
   },
@@ -29,13 +32,33 @@ function Header(): any {
   return <div className="w-[100%] h-[3rem] bg-[blue]"></div>;
 }
 function Form(): any {
+  const [quantity, setQuantity] = useState<number>(1);
+  const [description, setDescription] = useState<string>("");
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    const newItem = {
+      description,
+      quantity,
+      id: Date.now(),
+      packed: false,
+    };
+    console.log(newItem);
+  };
+
   return (
     <div className="w-[100%] h-[10rem] flex justify-center items-center">
-      <form className="w-[70%] h-[60%] ">
+      <form className="w-[70%] h-[60%] " onSubmit={handleSubmit}>
         <div className="text-center text-2xl">Place your order</div>
         <div className="w-[100%] h-[50%] bg-[#eae8e8] flex justify-center items-center gap-[20px]">
           <div className="w-[70px]  h-[50px]  flex justify-center items-center">
-            <select className="w-[60px] text-[12px] h-[40px] outine-no border rounded-md">
+            <select
+              value={quantity}
+              onChange={(e: any) => {
+                setQuantity(Number(e.target.value));
+              }}
+              className="w-[60px] text-[12px] h-[40px] outine-no border rounded-md"
+            >
               {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
                 <option value={num} key={num}>
                   {num}
@@ -49,6 +72,10 @@ function Form(): any {
               className="pl-[20px]"
               placeholder="input item..."
               type="text"
+              value={description}
+              onChange={(e: any) => {
+                setDescription(e.target.value);
+              }}
             />
           </div>
 
@@ -60,18 +87,26 @@ function Form(): any {
     </div>
   );
 }
-function ProductBought({ list }: any) {
+function ProductBought() {
   return (
-    <div className="w-[100%] h-[10rem] bg-[gray]">
+    <ul className="w-[100%] h-[10rem] bg-[gray] flex justify-center items-center">
       {products.map((el: any) => (
-        <div>
-          <Item />
-        </div>
+        <Item item={el} key={el.id} />
       ))}
-    </div>
+    </ul>
   );
 }
-function Item(): any {
-  return <div></div>;
+function Item({ item }: any) {
+  return (
+    <div className="w-[20%] h-[50%] flex justify-center items-center">
+      <li>
+        <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+          {item.quantity}
+          {item.description}
+        </span>
+        <button>❌</button>
+      </li>
+    </div>
+  );
 }
 function Footer(): any {}
